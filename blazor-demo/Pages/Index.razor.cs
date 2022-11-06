d14fb8e9969f97e5fd486c3a3c6c74c38e4d6ad0
Lars <lars.kraemer@live.de>
2022-11-06 01:18:41 +0100

    HCD-1: added blazor demo project

---
+++ blazor-demo/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using lkcode.hetznercloudapi;
+using Microsoft.AspNetCore.Components;
+
+namespace blazor_demo.Pages;
+
+public partial class Index
+{
+    [Inject]
+    IHetznerCloudService HetznerCloudService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        //this.HetznerCloudService.LoadToken("");
+    }
+}
