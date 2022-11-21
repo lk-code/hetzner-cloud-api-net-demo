605dd1048c22a23da96a0c14b8c008d1e977d4fc
Lars <lars.kraemer@live.de>
2022-11-21 11:58:31 +0100

    HCD-1: demo view for server

--- blazor-demo/Pages/Index.razor.cs
+++ blazor-demo/Pages/Index.razor.cs
@@ -1,4 +1,6 @@
-﻿using lkcode.hetznercloudapi;
+﻿using lkcode.hetznercloudapi.Instances;
+using lkcode.hetznercloudapi.Interfaces;
+using lkcode.hetznercloudapi.ParameterObjects.Sort;
 using Microsoft.AspNetCore.Components;
 
 namespace blazor_demo.Pages;
@@ -6,12 +8,23 @@ namespace blazor_demo.Pages;
 public partial class Index
 {
     [Inject]
-    IHetznerCloudService HetznerCloudService { get; set; } = null!;
+    IServerService ServerService { get; set; } = null!;
+
+    private List<Server> _servers = new List<Server>();
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
 
-        //this.HetznerCloudService.LoadToken("");
+        var servers = await this.ServerService.GetAllAsync(
+            1,
+            25,
+            new List<IFilter>
+            {
+                //new NameFilter("lk")
+            },
+            new ServerSort(ServerSortField.NAME, SortDirection.DESC));
+
+        _servers = servers.Items.ToList();
     }
 }
