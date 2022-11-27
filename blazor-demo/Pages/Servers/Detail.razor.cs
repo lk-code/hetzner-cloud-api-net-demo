060e5af0c7e3bbb236357a29a4ea9a42c3a219cf
Lars <lars.kraemer@live.de>
2022-11-27 14:42:49 +0100

    HCD-1: added some ui, server shutdown and log

--- blazor-demo/Pages/Servers/Detail.razor.cs
+++ blazor-demo/Pages/Servers/Detail.razor.cs
@@ -1,7 +1,10 @@
 ﻿using lkcode.hetznercloudapi.Exceptions;
 using lkcode.hetznercloudapi.Instances.Server;
+using lkcode.hetznercloudapi.Instances.ServerActions;
 using lkcode.hetznercloudapi.Interfaces;
 using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace blazor_demo.Pages.Servers;
 
@@ -13,9 +16,12 @@ public partial class Detail
     [Inject]
     IServerService ServerService { get; set; } = null!;
     [Inject]
+    IServerActionsService ServerActionsService { get; set; } = null!;
+    [Inject]
     NavigationManager NavigationManager { get; set; } = null!;
 
-    private bool IsLoading { get; set; } = false;
+    private bool IsBusy { get; set; } = false;
+    private StringBuilder LogStringBuilder { get; set; } = new();
     private Server Server { get; set; } = null!;
 
     protected override async Task OnInitializedAsync()
@@ -24,31 +30,65 @@ public partial class Detail
 
         try
         {
-            this.IsLoading = true;
+            this.Log("GET SERVER BY ID...");
+            this.IsBusy = true;
 
             // load server
             Server server = await this.ServerService.GetByIdAsync(this.Id);
 
             this.Server = server;
+            this.Log("SUCCESS - Finished");
         }
         catch (ResourceNotFoundException exception)
         {
             // server not found
+            this.Log("ERROR - ResourceNotFoundException:");
+            this.Log(exception.Message, false);
         }
         catch (Exception exception)
         {
             // display error
-
-            int i = 0;
+            this.Log("ERROR - Exception:");
+            this.Log(exception.Message, false);
         }
         finally
         {
-            this.IsLoading = false;
+            this.IsBusy = false;
         }
     }
 
     public void OnHomeClick()
     {
         NavigationManager.NavigateTo("/server");
+    }
+
+    public async Task OnServerShutdownClick()
+    {
+        try
+        {
+            this.Log("SHUTDOWN");
+            this.IsBusy = true;
+
+            // load server
+            ActionResult result = await this.ServerActionsService.Shutdown(this.Server.Id);
+
+            this.Log(JsonConvert.SerializeObject(result), false);
+            this.Log("SUCCESS - Finished");
+        }
+        catch (Exception exception)
+        {
+            // display error
+            this.Log("ERROR - Exception:");
+            this.Log(exception.Message, false);
+        }
+        finally
+        {
+            this.IsBusy = false;
+        }
+    }
+
+    private void Log(string message, bool displayDate = true)
+    {
+        this.LogStringBuilder.AppendLine($"{((displayDate) ? DateTime.Now.ToLongTimeString() + " - " : "")}{message}");
     }
 }
